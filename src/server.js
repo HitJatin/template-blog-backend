81793968dd19b57c15802df253b1ae51f7e53855
HitJatin <adriano23012001@gmail.com>
2021-11-09 21:50:59 +0530

    update

--- src/server.js
+++ src/server.js
@@ -1,7 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 
-app.get("/hello", (req, res) => res.send("Hello"));
+app.use(bodyParser.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+    try {
+        const articleName = req.params.name;
+        const client = await MongoClient.connect("mongodb://localhost:27017", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        const db = client.db("myblog");
+        const articleInfo = await db.collection("articles").findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+        client.close();
+    } catch (error) {
+        res.status(500).json({message: 'Error Connecting to db', error})
+    }
+})
+
+app.post('/api/articles/:name/add-comments', (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    articleInfo[articleName].comments.push({ username, text });
+    res.status(200).send(articleInfo[articleName]);
+})
 
 app.listen(8000, () => console.log("Listening on port 8000"));
